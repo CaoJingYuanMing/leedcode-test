d85737b6610897e5d67ae3616333112f0001cf67
banqiongyue <Qiongyue.Ban@externals.adidas.com>
2023-04-25 15:03:31 +0800

    【排序算法】: 推送框架

---
+++ src/main/java/org/example/sort/SortTest.java
@@ -0,0 +1,79 @@
+package org.example.sort;
+
+/**
+ * Top 10 sorting algorithm
+ * 十大排序算法
+ */
+
+public class SortTest {
+    public static void main(String[] args) {
+        System.out.println("===========排序算法题=========");
+    }
+
+    /**
+     * 冒泡排序
+     */
+    public static void bubbleSort() {
+        System.out.println("冒泡排序");
+    }
+
+    /**
+     * 选择排序
+     */
+    public static void selectSort() {
+        System.out.println("选择排序");
+    }
+
+    /**
+     * 插入排序
+     */
+    public static void insertionSort() {
+        System.out.println("插入排序");
+    }
+
+    /**
+     * 希尔排序
+     */
+    public static void shellSort() {
+        System.out.println("希尔排序");
+    }
+
+    /**
+     * 归并排序
+     */
+    public static void divideAndConquer() {
+        System.out.println("归并排序");
+    }
+
+    /**
+     * 快速排序
+     */
+    public static void quickSort() {
+        System.out.println("快速排序");
+    }
+
+    /**
+     * 堆排序
+     */
+    public static void heapsort() {
+        System.out.println("堆排序");
+
+    }
+
+    /**
+     * 桶排序
+     */
+    public static void bucketSort() {
+        System.out.println("桶排序");
+    }
+
+    /**
+     * 基数排序
+     */
+    public static void radixSort() {
+
+    }
+
+
+
+}
